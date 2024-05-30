c15bf0f1a45f0ea256d0b38c31c9bb5f29325c29
harshal31718 <harshaldodke31718@gmail.com>
2024-05-31 01:16:16 +0530

    implement basix upgrades

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,26 @@
+import BalanceBox from '@/components/BalanceBox';
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: "Harshal"};
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subText="Access and manage your accounts and Transactions."
+        />
+        <BalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={14324.43}
+        />
+      </div>
+    </section>
   )
 }
 
---
+++ components/AnimatedBalance.tsx
@@ -0,0 +1,18 @@
+'use client';
+import React from 'react'
+import CountUp from 'react-countup';
+
+const AnimatedBalance = ({amount}:{amount:number}) => {
+  return (
+    <div className='w-full'>
+        <CountUp 
+            prefix='$'
+            end={amount}
+            decimals={2}
+            decimal='.'
+        />
+    </div>
+  )
+}
+
+export default AnimatedBalance
