05c87fe10f651bc2a491455e4d78534017722746
harshal31718 <harshaldodke31718@gmail.com>
2024-05-31 12:08:32 +0530

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,38 @@
 import BalanceBox from '@/components/BalanceBox';
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstName: "Harshal"};
+  const loggedIn = {
+    firstName: "Harshal", 
+    lastName: "Dodke",
+    email: "harshal.dodke@gmail.com"
+  };
 
   return (
     <section className='home'>
       <div className='home-content'>
-        <HeaderBox 
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || "Guest"}
-          subText="Access and manage your accounts and Transactions."
-        />
-        <BalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={14324.43}
-        />
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subText="Access and manage your accounts and Transactions."
+          />
+          <BalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={14324.43}
+          />
+        </header>
+        {/* recent transactions */}
       </div>
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.4},{currentBalance: 504}]}
+      />
     </section>
   )
 }
